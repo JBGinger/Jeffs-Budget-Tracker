272df1eecfe7eef3d9a9e2a463dfdca12cbae887
jeffb <jeffb@bsdiweb.com>
2022-02-01 20:35:21 -0500

    updated idb

--- public/js/idb.js
+++ public/js/idb.js
@@ -7,17 +7,14 @@ request.onupgradeneeded = function (event) {
 };
 
 request.onsuccess = function (event) {
-    // when db is successfully created with its object store (from onupgradedneeded event above), save reference to db in global variable
     db = event.target.result;
 
-    // check if app is online, if yes run checkDatabase() function to send all local db data to api
     if (navigator.onLine) {
         UpdateBudget();
     }
 };
 
 request.onerror = function (event) {
-    // log error here
     console.log(event.target.errorCode);
 };
 
@@ -26,22 +23,17 @@ function saveRecord(record) {
 
     const budgetObjectStore = transaction.objectStore('new_transaction');
 
-    // add record to your store with add method.
     budgetObjectStore.add(record);
 };
 
 function UpdateBudget() {
-    // open a transaction on your pending db
     const transaction = db.transaction(['new_transaction'], 'readwrite');
 
-    // access your pending object store
     const budgetObjectStore = transaction.objectStore('new_transaction');
 
-    // get all records from store and set to a variable
     const getAll = budgetObjectStore.getAll();
 
     getAll.onsuccess = function () {
-        // if there was data in indexedDb's store, let's send it to the api server
         if (getAll.result.length > 0) {
             fetch('/api/transaction/bulk', {
                 method: 'POST',
@@ -59,11 +51,9 @@ function UpdateBudget() {
 
                     const transaction = db.transaction(['new_transaction'], 'readwrite');
                     const budgetObjectStore = transaction.objectStore('new_transaction');
-                    // clear all items in your store
                     budgetObjectStore.clear();
                 })
                 .catch(err => {
-                    // set reference to redirect back here
                     console.log(err);
                 });
         }
